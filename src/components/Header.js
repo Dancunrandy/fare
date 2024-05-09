fac40a2e489ef70954dba18e8850fbdf558c62cf
dancunrandy <dancunrandy@gmail.com>
2024-05-09 10:15:19 +0300

    header

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+// Header.js
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../index.css'; // Import your CSS file for styling
+const Header = () => {
+  return (
+    <header className="header">
+      <nav className="navbar">
+        <div className="container">
+          <Link to="/" className="logo">FairPay</Link>
+          {/* <ul className="nav-links"> */}
+            {/* <li><Link to="/">Login</Link></li> */}
+            {/* <li><Link to="/register">Register</Link></li> */}
+            {/* Add more navigation links here */}
+          {/* </ul> */}
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;

8598f9981180ad1b2386ba6d2c9c4e35f00847c6
Oduol Peris <oduolpepe618@gmail.com>
2024-05-22 14:06:02 +0300

    🎨feat: sending a checkout request, on success, IntaSend will respond with several data fields including an invoice_id

---
+++ Routes/Payment.route.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const createHttpError = require('http-errors');
+const router = express.Router();
+const IntaSend = require('intasend-node');
+const Matatu = require('../Models/Matatu.model');
+
+const intasend = new IntaSend(
+  process.env.INTASEND_PUBLISHABLE_KEY, 
+  process.env.INTASEND_SECRET_KEY, 
+  true
+);
+
+router.post('/mpesa', async (req, res, next) => {
+  try {
+    const { fleetNumber, numberPlate, firstName, lastName, email, phoneNumber, amount } = req.body;
+
+    if (!fleetNumber || !numberPlate || !firstName || !lastName || !email || !phoneNumber || !amount) {
+      throw createHttpError.BadRequest('All fields are required');
+    }
+
+    const matatu = await Matatu.findOne({ fleetNumber, numberPlate });
+
+    if (!matatu) {
+      throw createHttpError.NotFound('Matatu not found');
+    }
+
+    let collection = intasend.collection();
+    const paymentResponse = await collection.mpesaStkPush({
+      first_name: firstName,
+      last_name: lastName,
+      email: email,
+      phone_number: phoneNumber,
+      amount: amount,
+      api_ref: `Payment for ${fleetNumber} - ${numberPlate}`
+    });
+
+    res.status(200).json(paymentResponse);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;

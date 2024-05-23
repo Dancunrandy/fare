6c43e9a69e8a1619676a41ce33739dfe0b84cda4
dinhcaradoc <kwambugualex@gmail.com>
2024-05-23 19:30:03 +0300

    update route configurations and add functionality to handle session data storage

--- fare_ussd/ussd.js
+++ fare_ussd/ussd.js
@@ -1,15 +1,40 @@
 const UssdMenu = require('ussd-builder');
-const vehicle = require('./vehicles');
-const mpesa = require('./mpesa');
-const STKPush = require('./intasend')
+// const vehicle = require('./models/vehicles');
+const payments = require('./models/payments');
+const STKPush = require('./intasend');
+const redisClient = require('./config/redis'); // Import Redis configuration
 
-let sessions = {};
-let menu = new UssdMenu();
+const sessionPrefix = 'ussd_session_'; // Define session prefix
 
+const menu = new UssdMenu();
+
+// Function to get or create session
+const getSession = async (sessionId) => {
+  try {
+    const sessionData = await redisClient.hGetAll(sessionPrefix + sessionId);
+    if (!sessionData || Object.keys(sessionData).length === 0) {
+      return {}; // Initialize session data if not found
+    }
+    return sessionData;
+  } catch (err) {
+    console.error('Error retrieving session data from Redis:', err);
+    throw err;
+  }
+};
+
+// Function to set session data
+const setSession = async (sessionId, data) => {
+  try {
+    await redisClient.hSet(sessionPrefix + sessionId, data);
+  } catch (err) {
+    console.error('Error setting session data in Redis:', err);
+    throw err;
+  }
+};
 
 menu.startState({
   run: () => {
-    menu.con(`Welcome to TumaFare \n\n Please enter the vehicle fleet number:`);
+    menu.con('Welcome to TumaFare \n\nPlease enter the vehicle fleet number:');
   },
   next: {
     '*\\w': 'inputAmount'
@@ -17,11 +42,16 @@ menu.startState({
 });
 
 menu.state('inputAmount', {
-  run: () => {
-    let fleet = menu.val;
-    let session = getSession(menu.args.sessionId);
-    session.set('fleet_no', fleet);
-    menu.con(`Enter the amount`);
+  run: async () => {
+    const fleet = menu.val;
+    try {
+      const session = await getSession(menu.args.sessionId);
+      session.fleet_no = fleet;
+      await setSession(menu.args.sessionId, session);
+      menu.con('Enter the amount');
+    } catch (err) {
+      menu.end('Error saving session data');
+    }
   },
   next: {
     '*\\d+': 'confirmInput'
@@ -29,13 +59,19 @@ menu.state('inputAmount', {
 });
 
 menu.state('confirmInput', {
-  run: () => {
-    let amnt = menu.val;
-    let session = getSession(menu.args.sessionId);
-    session.set('amount', amnt);
-    menu.con(`You are about to pay ${amnt} to TumaFare Inc. Please confirm:
-        \n1. Confirm` +
-      `\n2. Cancel`)
+  run: async () => {
+    const amount = menu.val;
+    try {
+      const session = await getSession(menu.args.sessionId);
+      session.amount = amount;
+      await setSession(menu.args.sessionId, session);
+      fleet = session.fleet_no
+      menu.con(`You are about to pay ${amount} to TumaFare Inc. for your trip with ${fleet} Please confirm:
+                \n1. Confirm` +
+        `\n2. Cancel`);
+    } catch (err) {
+      menu.end('Error saving session data');
+    }
   },
   next: {
     '1': 'confirmPay',
@@ -46,13 +82,12 @@ menu.state('confirmInput', {
 menu.state('confirmPay', {
   run: async () => {
     try {
-      // Retrieve session data
-      let session = getSession(menu.args.sessionId);
-
-      // Prepare payment information
+      const session = await getSession(menu.args.sessionId);
+      let fare = session.amount
       const payInfo = {
         phone: menu.args.phoneNumber.substring(1), // Get phone number from USSD args
-        amount: 10 // Get amount from session data
+        amount: fare, // Get amount from session data
+        fleet: session.fleet_no
       };
 
       console.log('Payment Info:', payInfo);
@@ -60,29 +95,28 @@ menu.state('confirmPay', {
       // Initiate M-Pesa STK push payment using the payment service
       const paymentResponse = await STKPush.intaSTKPush(payInfo);
 
-      // Log payment response (optional)
+      // Log payment response (optional. used here for error checking)
       console.log('Payment Response:', paymentResponse);
 
       // End the USSD session with a confirmation message
       menu.end(`Thank you for using Tuma Fare service. Enter your MPesa pin when prompted.`);
-    } catch (error) {
+    } catch (err) {
       // Log and handle any errors
-      console.error('Error confirming payment:', error);
+      console.error('Error confirming payment:', `${JSON.stringify(err)}`);
       // End the USSD session with an error message
       menu.end(`An error occurred. Please try again later.`);
     }
   }
 });
 
-
 menu.state('cancelPay', {
   run: () => {
-    menu.end(`You have cancelled the transaction. Thank you for using Tuma Fare.`)
+    menu.end('You have cancelled the transaction. Thank you for using Tuma Fare.');
   }
 });
 
 exports.initUssd = async (req, res) => {
-  let args = {
+  const args = {
     phoneNumber: req.body.phoneNumber,
     sessionId: req.body.sessionId,
     serviceCode: req.body.serviceCode,

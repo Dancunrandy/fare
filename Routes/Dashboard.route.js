a1413b50b1e35e87defa5cdde661b966a56977c6
Oduol Peris <oduolpepe618@gmail.com>
2024-05-23 19:16:22 +0300

    🐛bug:solve bug

--- Routes/Dashboard.route.js
+++ Routes/Dashboard.route.js
@@ -4,12 +4,11 @@ const router = express.Router();
 const IntaSend = require('intasend-node');
 const Matatu = require('../Models/Matatu.model');
 
-require('dotenv').config();  
-
+require('dotenv').config();
 
 const intasend = new IntaSend(
-  process.env.INTASEND_PUBLISHABLE_KEY, 
-  process.env.INTASEND_SECRET_KEY, 
+  process.env.INTASEND_PUBLISHABLE_KEY,
+  process.env.INTASEND_SECRET_KEY,
   true
 );
 
@@ -24,10 +23,10 @@ router.get('/dashboard', async (req, res, next) => {
     let query = {};
     if (fleetNumber) query.fleetNumber = fleetNumber;
     if (numberPlate) query.numberPlate = numberPlate;
-    console.log("Query:",query)
+    console.log('Query:', query);
 
     const matatu = await Matatu.findOne(query);
-    console.log("Matatu:", matatu)
+    console.log('Matatu:', matatu);
 
     if (!matatu) {
       throw createHttpError.NotFound('Matatu not found');
@@ -37,26 +36,26 @@ router.get('/dashboard', async (req, res, next) => {
     const transactionsResponse = await wallets.transactions(matatu.walletId);
 
     let totalIncome = 0;
-    console.log(totalIncome)
+    console.log(totalIncome);
     if (Array.isArray(transactionsResponse)) {
       totalIncome = transactionsResponse.reduce((total, transaction) => {
         return total + parseFloat(transaction.value);
       }, 0);
     }
-    
-    
+
     res.status(200).json({
       fleetNumber: matatu.fleetNumber,
       numberPlate: matatu.numberPlate,
+      sacco: matatu.sacco,  // Include the sacco in the response
       totalIncome: totalIncome,
-      transactions: transactionsResponse
+      transactions: transactionsResponse,
     });
-
-
   } catch (error) {
-      console.error(`Error:`, `${JSON.stringify(error)}`)
+    console.error(`Error:`, `${JSON.stringify(error)}`);
     next(error);
   }
 });
+
+
 
 module.exports = router;
